5c7690450cbd4af9f287d43076ff83cf27eac3c1
Robert Bartlett-Schneider <robert@savetherobots.org>
2011-01-09 14:46:48 -0800

    xcode project added

---
+++ EASy68K/MyDocument.h
@@ -0,0 +1,15 @@
+//
+//  MyDocument.h
+//  EASy68K
+//
+//  Created by Robert Bartlett-Schneider on 1/9/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+
+#import <Cocoa/Cocoa.h>
+
+@interface MyDocument : NSDocument
+{
+}
+@end
---
+++ EASy68K/main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  EASy68K
+//
+//  Created by Robert Bartlett-Schneider on 1/9/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, char *argv[])
+{
+    return NSApplicationMain(argc, (const char **) argv);
+}
